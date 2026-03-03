bff90c031b458b4742ce42593f3097bd6c6836f9
zaliarhm2007-code <zaliarhm2007@gmail.com>
2026-03-03 16:34:15 +0700

    p2

--- minggu1/Zahra05.java
+++ minggu1/Zahra05.java
@@ -5,5 +5,25 @@ public class Zahra05 {
     String nim;
     String kelas;
     double ipk;
-    
+
+    void ubahKelas (String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk (double ipkBaru) {
+        ipk = ipkBaru
+    }
+
+    String nilaiKinerja() {
+    if (ipk >= 3.5) {
+        return "Kinerja sangat baik";
+    } else if (ipk >= 3.0) {
+        return "Kinerja baik";
+    } else if (ipk >= 2.0) {
+        return "Kinerja cukup";
+    } else {
+        return "Kinerja kurang";
+    }
+}
+
 }
